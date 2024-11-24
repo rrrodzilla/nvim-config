77e87ec998da7fb87980c45ab41ff899dd9d1421
Roland Rodriguez <roland@govcraft.ai>
2024-11-23 22:56:06 -0600

    fix(plugin): avante

--- packages/rrrodzilla/plugins/avante-nvim.nix
+++ packages/rrrodzilla/plugins/avante-nvim.nix
@@ -3,11 +3,11 @@
     avante = {
       enable = true;
       settings = {
-        provider = "openai";
+        provider = "claude";
         auto_suggestions_provider = "claude";
         openai = {
-          endpoint = "https://api.openai.com/v1";
-          model = "gpt-4o";
+          endpoint = "https://api.anthropic.com";
+          model = "claude-3-5-sonnet-20241022";
           temperature = 0;
           max_tokens = 4096;
         };
